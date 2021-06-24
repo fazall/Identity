45e729f05b9cf773d247c91df58301859b3922d0
fazall <shkinwall@gmail.com>
2021-06-24 22:17:49 +0430

    controller and views are added

---
+++ IdentityExample/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+
+            return View("Index");
+        }
+
+        public IActionResult Login(string userName, string password)
+        {
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Register()
+        {
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Register(string userName, string password)
+        {
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- IdentityExample/Startup.cs
+++ IdentityExample/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Basec
+namespace IdentityExample
 {
     public class Startup
     {
